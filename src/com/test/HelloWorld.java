d536eb01f531f739a54717b273e63246f36f363d
eswar <eswarpalacharla@gmail.com>
2020-04-12 00:13:42 +0530

    2nd commit from eclipse egit

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.test;
 public class HelloWorld {
     public static void main(String[] args) {
 		System.out.println("welcome to git command line");
+		System.out.println("Welcome to git I am commiting from IDE 1ST time");
 	}
 }
