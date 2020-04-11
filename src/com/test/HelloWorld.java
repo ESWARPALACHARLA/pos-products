ab043f75484515fadb37381c853cd12a9867df0d
eswar <eswarpalacharla@gmail.com>
2020-04-12 00:20:46 +0530

    commiting ticket changes locally

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 		System.out.println("welcome to git command line");
 		System.out.println("Welcome to git I am commiting from IDE 1ST time");
+		System.out.println("add new branch ticket and commiting 1st time");
 	}
 }
