3f3d346cd2bb7c5bb2d84a988f01268066c0d30c
eswar <eswarpalacharla@gmail.com>
2020-04-12 10:14:55 +0530

    added new file committing

---
+++ src/com/test/Example.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Example {
+	
+	  public static String appendCP(String name){
+		  return name.concat("CP");
+	  }
+
+	public static void main(String[] args) {
+		appendCP("ESWAR");
+	}
+}
