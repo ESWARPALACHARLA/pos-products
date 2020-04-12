fa72104d8b2438864b992ce566128c99d7e89285
eswar <eswarpalacharla@gmail.com>
2020-04-12 10:17:38 +0530

    added 2nd method to commit

--- src/com/test/Example.java
+++ src/com/test/Example.java
@@ -5,7 +5,9 @@ public class Example {
 	  public static String appendCP(String name){
 		  return name.concat("CP");
 	  }
-
+	  public static String replaceSpecialCharacters(String name){
+		  return name.replace("&", "&amp");
+	  }
 	public static void main(String[] args) {
 		appendCP("ESWAR");
 	}
